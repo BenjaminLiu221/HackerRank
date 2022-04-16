3e8da8ec1d0e9ed7d329ba161395f068895be34a
Benjamin Liu <benjaminliu221@gmail.com>
2022-04-16 15:20:08 -0700

    completed min max sum

--- Challenges/MiniMaxSum/MiniMaxSum/MiniMaxSum/Program.cs
+++ Challenges/MiniMaxSum/MiniMaxSum/MiniMaxSum/Program.cs
@@ -23,7 +23,37 @@ class Result
 
     public static void miniMaxSum(List<int> arr)
     {
+        List<long> listArr = new List<long>();
 
+        long smallestSum = 0;
+        long largestSum = 0;
+        foreach (long num in arr)
+        {
+            listArr.Add(num);
+        }
+
+        long smallestNum = listArr.Min();
+        long largestNum = listArr.Max();
+
+        int indexOfSmallestNum = listArr.IndexOf(smallestNum, 0, listArr.Count);
+
+        int indexOfLargestNum = listArr.IndexOf(largestNum, 0, listArr.Count);
+
+        for (int i = 0; i < listArr.Count; i++)
+        {
+            if (i != indexOfSmallestNum)
+            {
+                largestSum += listArr[i];
+            }
+        }
+        for (int i = 0; i < listArr.Count; i++)
+        {
+            if (i != indexOfLargestNum)
+            {
+                smallestSum += listArr[i];
+            }
+        }
+        Console.WriteLine($"{smallestSum} {largestSum}");
     }
 
 }
